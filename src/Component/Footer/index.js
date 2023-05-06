e419a1af20dab86ca79e0762ee531cc95867ce31
Rupesh Sharma <116899025+Gitrupesh20@users.noreply.github.com>
2023-05-06 01:30:42 +0000

    Update 5 files

--- src/Component/Footer/index.js
+++ src/Component/Footer/index.js
@@ -1,27 +1,34 @@
-import './Footer.css'
+import "./Footer.css";
 
 export default function Footer() {
-    return (
-        <div className='footer'>
-            <div className='footersec' id='upfooter'>
-                <img src={process.env.PUBLIC_URL + '/assets/logo2.png'} alt="logo" style={{width:"100px", height:"30px"}}/>
-                <section>
-                    <i class="fab fa-facebook"></i>
-                    <i class="fab fa-instagram"></i>
-                    <i class="fas fa-at"></i>
-                    <i class="fab fa-telegram"></i>
-                </section>
-            </div>
-            <hr />
-            <div className='footersec' id='downfooter'>
-                <p>© BLOOD-DONOR 2021. We love our users!</p>
-                <section>
-                    <a href="#">Privacy & Policies</a>
-                    <a href="#">Terms & Conditions</a>
-                    <a href="/about">About Us</a>
-                    <a href="/#contact">Contact Us</a>
-                </section>
-            </div>
-        </div>
-    )
+  return (
+    <div className="footer">
+      <div className="footersec" id="upfooter">
+        <img
+          src={
+            process.env.PUBLIC_URL +
+            "/assets/20230506_001728__1_-removebg-preview.png"
+          }
+          alt="logo"
+          style={{ width: "100px", height: "30px" }}
+        />
+        <section>
+          <i class="fab fa-facebook"></i>
+          <i class="fab fa-instagram"></i>
+          <i class="fas fa-at"></i>
+          <i class="fab fa-telegram"></i>
+        </section>
+      </div>
+      <hr />
+      <div className="footersec" id="downfooter">
+        <p>© BLOOD-DONOR 2021. We love our users!</p>
+        <section>
+          <a href="#">Privacy & Policies</a>
+          <a href="#">Terms & Conditions</a>
+          <a href="/about">About Us</a>
+          <a href="/#contact">Contact Us</a>
+        </section>
+      </div>
+    </div>
+  );
 }
